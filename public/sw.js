5030f969a962320270452590c07c700a652b106d
Thiago <thiago@sygmasistemas.com.br>
2026-04-22 15:59:17 -0300

    feat: novo ícone com gradiente roxo no padrão dos outros apps

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="financas-v14";
+const CACHE="financas-v15";
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["/","/index.html"])));
   self.skipWaiting();
